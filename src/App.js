a63a877060e0d4ed817dcb9aea95cdec1a160b0a
jinnymoon1124 <jinnymoon1124@gmail.com>
2023-01-18 22:20:08 +0900

    publishing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Switch>
+        <Route path="hello">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
+
+/*
+1. package.json에 있는 scripts를 확인
+2. package.json에는 build라는 script가 존재
+3. 해당 script를 실행 (npm run build) 하면 웹사이트의 production ready가 되며, 이 과정은 코드가 압축되고 모든게 최적화 된다는 의미
+4. 
+
+
+
+
+*/
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,24 +1,29 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-function Movie({coverImg, title, summary, genres}) { // movies 컴포넌트가 중괄호 안의 정보들을 부모 컴포넌트로부터 받아오는 것을 의미
-    return <div>
-    <img src = {coverImg} alt={title}/>
-    <h2>{title}</h2>
-    <p>{summary}</p>
-    <ul>
-      {genres.map(g => (
-        <li key={g}>{g}</li>
-      ))}
-    </ul>
-    <hr />
-  </div>
+function Movie({ id, coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 Movie.propTypes = {
-    coverImg : PropTypes.string.isRequired,
-    title : PropTypes.string.isRequired,
-    summary : PropTypes.string.isRequired,
-    genres : PropTypes.arrayOf(PropTypes.string).isRequired,
+  id: PropTypes.number.isRequired,
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
